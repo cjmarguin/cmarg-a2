fbad25319e67f858eeb4b99122830b5f1bada785
Claudia "CJ" Marguin <marguin_claudia@columbusstate.edu>
2019-11-24 21:17:14 +0000

    jumbo 1

--- content/home.php
+++ content/home.php
@@ -2,4 +2,4 @@
 
 
 <div class="d-flex justify-content-center"><div > <?php getComic(); ?> </div></div>
-<!--<div class="d-flex justify-content-center"><img src = <//?php getComicImage(); ?>></div>-->
+<div class="d-flex justify-content-center"><img src = <//?php getComicImage(); ?>></div>
