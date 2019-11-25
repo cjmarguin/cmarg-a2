9041ac33caf066d03bb01a3907a43cac57675b0d
Claudia "CJ" Marguin <marguin_claudia@columbusstate.edu>
2019-11-25 22:00:45 +0000

    functions

--- content/api.php
+++ content/api.php
@@ -1,30 +1,4 @@
-
-<script>
-           $(document).ready(function(){
-  $("button").click(function(){
-                 //location.reload();
-                $.ajax({
-                    type: "post",
-                    url: "/includes/randomFunction.php",
-                    data: {'random': true},
-                    dataType: "HTML",
-                    success: function (data) {
-                        document.getElementById('randomComic').innerHTML = data;
-                    },
-                    error: function (result) {
-                        alert("Error");
-                    }
-                });
-                return false;
-            });
-           });
-        </script>
-<div style="margin-right: 0px; margin-left: 0px; text-align: center;">
-<button  id="change">New Comic</button>
-<div class="d-flex justify-content-center">
+<button id="button" class="btn btn-outline-primary">Get new comic</button>
 <br>
-<div id="randomComic">
-<div> <?php getRandCom(); ?> </div>
-</div>
-</div>
-</div>
+<br>
+<div id="randomPic"><?php getRandCom(); ?></div>
