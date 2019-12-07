3d380624a70201d0b9af6b460c649f80d5ee1dac
Claudia "CJ" Marguin <marguin_claudia@columbusstate.edu>
2019-12-07 08:30:40 +0000

    fix home layout 4

--- content/api.php
+++ content/api.php
@@ -1,4 +1,4 @@
-<button id="button" class="btn btn-outline-primary">Get new comic</button>
+<button id="button" class="btn btn-outline-primary">Shuffle comic</button>
 <br>
 <br>
 <div id="randomPic"><?php getRandCom(); ?></div>
